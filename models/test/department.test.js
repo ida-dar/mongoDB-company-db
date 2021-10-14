e45e9ad00e0ac2191feaf4f855ee786eaec2a6d8
Ida Darmochwał <ida.mdart@gmail.com>
2021-10-14 18:48:11 +0200

    Add tests for employee and department

---
+++ models/test/department.test.js
@@ -0,0 +1,52 @@
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+const Department = require('../department.model.js');
+
+describe('Department', () => {
+
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); // create new Department without `name` attr value
+
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    })
+  });
+
+  it('should throw an error if "name" is not a string', () => {
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('shoulg throw an error if "name" is longer than 20 or shorter than 5', () => {
+    const cases = ['name', 'Loremipsumloremipsumloremipsum'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should NOT throw error if "name" is correct', () => {
+    const cases = ['Design', 'Lorem ipsum'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+
+});
+
+after(() => {
+  mongoose.models = {};
+});
---
+++ models/test/employee.test.js
@@ -0,0 +1,86 @@
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+const Employee = require('../employee.model.js');
+
+describe('Employee', () => {
+
+  it('should throw an error if no args', () => {
+    const emp = new Employee({});
+
+    emp.validate(err => {
+      expect(err.errors.firstName).to.exist &&
+      expect(err.errors.lastName).to.exist &&
+      expect(err.errors.department).to.exist;
+    })
+  });
+
+  it('should throw an error if args are not a string', () => {
+    const cases = [{}, []];
+    for(let arg of cases) {
+      const emp = new Employee({ arg, arg, arg });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.exist &&
+        expect(err.errors.lastName).to.exist &&
+        expect(err.errors.department).to.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "firstName" arg is missing', () => {
+    const cases = [{}, []];
+    for(let arg of cases) {
+      const emp = new Employee({ firstName: arg, lastName: 'lorem', department: 'lorem' });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.exist &&
+        expect(err.errors.lastName).to.not.exist &&
+        expect(err.errors.department).to.not.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "lastName" arg is missing', () => {
+    const cases = [{}, []];
+    for(let arg of cases) {
+      const emp = new Employee({ firstName: 'lorem', lastName: arg, department: 'lorem' });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.not.exist &&
+        expect(err.errors.lastName).to.exist &&
+        expect(err.errors.department).to.not.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "department" arg is missing', () => {
+    const cases = [{}, []];
+    for(let arg of cases) {
+      const emp = new Employee({ firstName: 'lorem', lastName: 'lorem', department: arg });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.not.exist &&
+        expect(err.errors.lastName).to.not.exist &&
+        expect(err.errors.department).to.exist;
+      });
+    }
+  });
+
+  it('should NOT throw an error if all args are corect', () => {
+    const cases = ['Lorem', 'Lorem ipsum'];
+    for(let arg of cases) {
+      const emp = new Employee({ arg, arg, arg });
+  
+      emp.validate(err => {
+        expect(err.errors.firstName).to.exist &&
+        expect(err.errors.lastName).to.exist &&
+        expect(err.errors.department).to.exist;
+      });
+    }
+  });
+
+});
+
+after(() => {
+  mongoose.models = {};
+});
